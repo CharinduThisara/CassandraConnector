4900639fc3012fdb4fc7d2d418155aed68391b78
CharinduThisara <thisaraimc@gmail.com>
2024-03-20 12:03:45 +0530

    Update .gitignore and pom.xml, add dotenv-java dependency, and connect to CosmosDB

--- src/main/java/cassandra/connector/CassandraConnector.java
+++ src/main/java/cassandra/connector/CassandraConnector.java
@@ -2,35 +2,59 @@ package cassandra.connector;
 
 import java.net.InetSocketAddress;
 import java.util.Map;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.CqlIdentifier;
 
 import com.datastax.oss.driver.api.core.DriverException;
 import com.datastax.oss.driver.api.core.CqlSessionBuilder;
-import com.datastax.oss.driver.api.core.cql.PreparedStatement;
-import com.datastax.oss.driver.api.core.uuid.Uuids;
+import javax.net.ssl.*;
+
+import org.apache.tinkerpop.shaded.kryo.util.IdentityMap.Keys;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 public class CassandraConnector {
 
     private CqlSession session;
-    private String systemUserPrefix = "system_";
     private static final String NODE_IP = "127.0.0.1";
     private static final int PORT = 9042;
-    private static final String KEYSPACE = "my_keyspace";
     private static final String LOCAL_DATACENTER = "datacenter1";
     private static final String INSERT_USER_QUERY = "INSERT INTO users (user_id, username, credential, role_list, claims, profile) VALUES (?, ?, ?, ?, ?, ?)";
-    String createKeyspaceQuery = "CREATE KEYSPACE IF NOT EXISTS my_keyspace "
+    private String createKeyspaceQuery = "CREATE KEYSPACE IF NOT EXISTS my_keyspace "
             + "WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};";
 
-    String createTableQuery = "CREATE TABLE IF NOT EXISTS my_keyspace.users ("
+    private String createTableQuery = "CREATE TABLE IF NOT EXISTS my_keyspace.users ("
             + "user_id UUID PRIMARY KEY, "
             + "username TEXT, "
             + "credential BLOB, "
             + "role_list SET<TEXT>, "
             + "claims MAP<TEXT, TEXT>, "
             + "profile TEXT)";
-    public void connect(String node, Integer port, String dataCenter) {
+
+    private static String cassandraHost;
+    private static int cassandraPort;
+    private static String region;
+    private static String cassandraUsername;
+    private static String cassandraPassword;
+    public CassandraConnector() {
+        Dotenv dotenv = Dotenv.configure().load();
+        cassandraHost = dotenv.get("COSMOSDB_HOST");
+        cassandraPort = Integer.parseInt(dotenv.get("COSMOSDB_PORT"));
+        region = dotenv.get("COSMOSDB_REGION");
+        cassandraUsername = dotenv.get("COSMOSDB_USERNAME");
+        cassandraPassword = dotenv.get("COSMOSDB_PASSWORD");
+    }
+
+    public void connectLocal(String node, Integer port, String dataCenter) {
         CqlSessionBuilder builder = CqlSession.builder();
         builder.addContactPoint(new InetSocketAddress(node, port));
         builder.withLocalDatacenter(dataCenter);
@@ -38,6 +62,29 @@ public class CassandraConnector {
         this.session = builder.build();
 
         System.out.println("Connected to Cassandra");
+    }
+
+    public void connectCosmos() {
+
+        SSLContext sc = null;
+        try{
+
+            final KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+
+            final TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+
+            sc = SSLContext.getInstance("TLSv1.2");
+            sc.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            this.session = CqlSession.builder().withSslContext(sc)
+            .addContactPoint(new InetSocketAddress(cassandraHost, cassandraPort)).withLocalDatacenter(region)
+            .withAuthCredentials(cassandraUsername, cassandraPassword).build();
+        }
+        catch (Exception e) {
+            System.out.println("Error creating session");
+            e.printStackTrace();
+        }
+        
+
     }
 
     public void useKeyspace(String keyspace) {
@@ -48,7 +95,8 @@ public class CassandraConnector {
         System.out.println("Connecting Cassandra");
         CassandraConnector connector = new CassandraConnector();
         try {
-            connector.connect(NODE_IP, PORT, LOCAL_DATACENTER);}
+            connector.connectCosmos();
+        }
         catch (DriverException e) {
             System.out.println("Error connecting to Cassandra");
             e.printStackTrace();
@@ -63,6 +111,14 @@ public class CassandraConnector {
             e.printStackTrace();
         }
 
+        try {
+            connector.session.execute(connector.createTableQuery);
+            System.out.println("Table created");
+        }
+        catch (DriverException e) {
+            System.out.println("Error creating table");
+            e.printStackTrace();
+        }
 
     }
 
